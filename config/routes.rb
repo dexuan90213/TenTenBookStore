aa0a81b83bbfe24f513f0cfc33ce7ecd375ed022
Eden Yang <dexuan90213@gmail.com>
2019-11-26 15:44:12 +0800

    add admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   root 'books#index'
+
+  namespace :admin do
+    resources :books # /admin/books
+    root 'books#index' # /admin/
+  end
 end
