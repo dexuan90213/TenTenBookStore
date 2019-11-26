d7ddc314c9f3a74f2f353350a380de37551a1091
Eden Yang <dexuan90213@gmail.com>
2019-11-26 12:29:58 +0800

    add kaminari paginate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,8 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.available.with_attached_cover_image
+    # @books = Book.where(on_sell: true).with_attached_cover_image
+    @books = Book.available.with_attached_cover_image.page(params[:page]).per(4)
   end
 
   def show
