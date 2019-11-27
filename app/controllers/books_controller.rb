945491147c6e0fa3f5774c4eba86ae7d588d86ec
Eden Yang <dexuan90213@gmail.com>
2019-11-27 16:02:24 +0800

    add side bar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   def index
     # @books = Book.where(on_sell: true).with_attached_cover_image
-    @books = Book.available.with_attached_cover_image.page(params[:page]).per(4)
+    @books = Book.available.with_attached_cover_image.page(params[:page]).per(6)
   end
 
   def show
