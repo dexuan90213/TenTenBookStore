f25de691236e0b21841424950f93f12584009f41
Eden Yang <dexuan90213@gmail.com>
2019-12-09 17:24:53 +0800

    add book comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,39 @@
 class BooksController < ApplicationController
-  before_action :find_book, only: [:show]
+  before_action :find_book, only: [:show, :comment]
   layout 'book'
 
   def index
-    # @books = Book.where(on_sell: true).with_attached_cover_image
     @books = Book.available.with_attached_cover_image.page(params[:page]).per(6)
     @publishers = Publisher.available
   end
 
   def show
+    @comment = Comment.new
+  end
+
+  def comment
+    # comment 角度
+    # @comment = Comment.new(comment_params, user: current_user, book: @book)
+    
+    # book 角度
+    @comment = @book.comments.build(comment_params)
+
+    # user 角度
+    # @comment = current_user.comments.build(comment_params, book: @book)
+
+    if @comment.save
+      redirect_to @book, notice: '留言成功'
+    else
+      # 失敗
+    end
   end
 
   private
   def find_book
     @book = Book.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :content).merge(user: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show] do
+    member do
+      post :comment # POST /books/:id/comment, to 'books#comment'
+    end
+  end
   resources :publishers, only: [:show]
+  # resources :comments, only: [:create]
 
   root 'books#index'
 
