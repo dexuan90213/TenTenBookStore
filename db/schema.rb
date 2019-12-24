9df7405aaee68ed062a2ce3943e793f24450fda7
Eden Yang <dexuan90213@gmail.com>
2019-12-24 12:04:14 +0800

    Profile

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_12_23_070447) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.date "publish_date"
     t.decimal "list_price"
     t.decimal "sell_price"
@@ -48,8 +51,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "published_at"
-    t.integer "publisher_id"
-    t.integer "category_id"
+    t.bigint "publisher_id"
+    t.bigint "category_id"
     t.datetime "deleted_at"
     t.index ["category_id"], name: "index_books_on_category_id"
     t.index ["deleted_at"], name: "index_books_on_deleted_at"
@@ -67,8 +70,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "book_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
@@ -76,8 +79,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "book_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_favorites_on_book_id"
@@ -85,8 +88,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "book_id", null: false
+    t.bigint "order_id", null: false
+    t.bigint "book_id", null: false
     t.integer "quantity"
     t.decimal "sell_price"
     t.datetime "created_at", precision: 6, null: false
@@ -96,7 +99,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_070447) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "recipient"
     t.string "tel"
     t.string "address"
